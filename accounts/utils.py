743600046a2b0d1d91b0c3b5eaf8c75e5805e40f
Bakdolot <genry03gh@gmail.com>
2021-12-09 16:15:56 +0600

    added celery broker

--- accounts/utils.py
+++ accounts/utils.py
@@ -39,10 +39,11 @@ def send_message_code(id, code, phone):
 
 def telegram_bot_sendtext(bot_message):
 
-   bot_token = settings.BOT_TOKEN
-   bot_chatID = settings.BOT_CHAT_ID
-   send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chatID + '&parse_mode=Markdown&text=' + bot_message
+    bot_token = settings.BOT_TOKEN
+    bot_chatID = settings.BOT_CHAT_ID
+    for chat_id in bot_chatID:
+        send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + chat_id + '&parse_mode=Markdown&text=' + bot_message
 
-   response = requests.get(send_text)
+        response = requests.get(send_text)
 
-   return response.json()
+    return response.json()
